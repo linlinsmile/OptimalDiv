387ee290d5ced03400f540488d046be65f4df806
Jacques Rioux <jacques.rioux@outlook.com>
2019-06-13 09:55:59 -0400

    Added some hints at lacking documentation in the code

--- optim.R
+++ optim.R
@@ -28,16 +28,25 @@ lhp1 = 1 - lhp
 eah  = exp(-alpha * h)
 
 x    = seq(from = 0, to = B, by = h)
+
+# Why 300 here? Give it a meaningful
+# name and set it at the top,
+# with the other defined constants
 Vold = 300 * rep(1, length(x))
+
 Vnew    = vector()
 
+# Explain what this outer loop is doing
 for (t in 1:nSteps){ 
    Vnew[1] = echp * Vold[2]
   
+   # Explain what this inner loop is doing
    for (k in 2:(length( Vold ) - 1)){
       m    = 1:(k-1)   
       Vsum = sum(Vold[k-m] * (1 - eah) *  eah^m)
       
+      # What is this test about?
+      # What does it mean? Why?
       if((lhp1 * Vold[k + 1] + Vsum * lhp ) * echp > Vold[k - 1] + h){
          Vnew[k] = ( lhp1 * Vold[k + 1] + Vsum * lhp ) * echp
       }
